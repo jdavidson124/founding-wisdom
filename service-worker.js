10470dd512c0cf444f4d6e322d2d372b21eb6d50
Joey Davidson <joe.davidson124@gmail.com>
2026-03-07 11:27:02 -0800

    Bump cache to founding-wisdom-v3

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'american-gazette-v2';
+const CACHE_NAME = 'founding-wisdom-v3';
 
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(clients.claim()));
